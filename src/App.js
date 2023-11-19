beb47721d8b6b2d9fec875457dc395a69e0d3926
Nemo21 <saggubableen9@gmail.com>
2023-11-19 20:42:29 +0530

    api key safety

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import axios from "axios";
 function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=f069fd4426545fb4bc6c93f4d5bedead`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.REACT_APP_BRUH_API_KEY}`;
   const searchLocation = (event) => {
     if (event.key === "Enter") {
       axios.get(url).then((response) => {
